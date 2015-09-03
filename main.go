108de7c25536219ba590789d1440af8f6e49f759
papercompute <papercompute@gmail.com>
2015-09-03 13:23:35 +0300

    hack

--- main.go
+++ main.go
@@ -4,13 +4,38 @@ import (
 	"net/http"
 	"log"
 	"runtime"
+  "fmt"
 "crypto/md5"
     "encoding/hex"
 //	"io"
   "io/ioutil"
   "encoding/json"
+  "encoding/gob"
+  "time"
 //	"regexp"
+  "github.com/boltdb/bolt"
+  "bytes"
+//  "runtime"
 )
+
+var db *bolt.DB
+
+func UpdKV(key []byte, value []byte, bucket []byte) error {
+  return db.Update(func(tx *bolt.Tx) error {
+    b := tx.Bucket(bucket)
+    return b.Put(key, value)
+  })
+}
+func GetKV(key []byte, bucket []byte) (error, []byte) {
+  var result []byte = nil
+  err := db.View(func(tx *bolt.Tx) error {
+    if b := tx.Bucket(bucket); b != nil {
+      result = b.Get(key)
+    }  
+    return nil
+  })
+  return err, result
+}
 
 
 func GetMD5Hash(text string) string {
@@ -54,34 +79,84 @@ func ReadJSON(r *http.Request, v interface{}) error {
 type Faas struct {}
 
 type User struct {
-  Id uint
-  Email string
-  PasswordHash string
+  Id uint `json:"id"`
+  Created time.Time  `json:"created"` // time.Now().Unix()
+  Email string  `json:"email"`
+  PasswordHash string `json:"-"`
 }
+
+/*
+ var decoded map[string] interface{}
+        err := json.Unmarshal(v, &decoded)
+        if err != nil {
+            return err
+        }
+        fmt.Printf("Title: %s\n", decoded["Title"])
+*/        
+
+func (u *User) GobEncode() ([]byte, error) {
+    w := new(bytes.Buffer)
+    encoder := gob.NewEncoder(w)
+    err := encoder.Encode(u.Id)
+    if err!=nil {
+        return nil, err
+    }
+    err = encoder.Encode(u.Created)
+    if err!=nil {
+        return nil, err
+    }
+    err = encoder.Encode(u.Email)
+    if err!=nil {
+        return nil, err
+    }
+    err = encoder.Encode(u.PasswordHash)
+    if err!=nil {
+        return nil, err
+    }
+    return w.Bytes(), nil
+}
+
+func (u *User) GobDecode(buf []byte) error {
+    r := bytes.NewBuffer(buf)
+    decoder := gob.NewDecoder(r)
+    err := decoder.Decode(&u.Id)
+    if err!=nil {
+        return err
+    }
+    err = decoder.Decode(&u.Created)
+    if err!=nil {
+        return err
+    }
+    err = decoder.Decode(&u.Email)
+    if err!=nil {
+        return err
+    }
+    return decoder.Decode(&u.PasswordHash)
+}
+
+
+
 
 type NewUserPost struct {
   Email string `json:"email"`
   Password string `json:"password"`
 }
-
-type UserGet struct {
+type UserGetReq struct {
+  Email string `json:"email"`
+}
+type UserGetResp struct {
+  Id uint `json:"id"`
+  Created time.Time `json:"created"`
   Email string `json:"email"`
 }
 
-var userIdCounter uint = 1;
-var userEmailStore = map[string]*User{}
-var userIdStore = map[uint]*User{}
+var userIdCounter uint = 127 
 
-func saveStoresToFile() error{
-  b, err := json.MarshalIndent(userEmailStore, "", "  ")
-  if err != nil {
-    log.Printf("saveStoresToFile error %s",err)
-    return err
-  }
-  ioutil.WriteFile("users",b,0644); 
-  return nil
+func GetUserId() uint{
+  uid := userIdCounter
+  userIdCounter = userIdCounter + 1
+  return uid  
 }
-
 
 func postNewUser(w http.ResponseWriter, r *http.Request){
   userPost:=NewUserPost{}
@@ -92,7 +167,6 @@ func postNewUser(w http.ResponseWriter, r *http.Request){
     return
   }
 
-  log.Printf("userPost{%s,%s}",userPost.Email,userPost.Password)
   if len(userPost.Email) < 4 { // a@a.co
     WriteStatus(w,500,"api newuser email error")
     return
@@ -102,24 +176,38 @@ func postNewUser(w http.ResponseWriter, r *http.Request){
     return
   }
 
-  if u:=userEmailStore[userPost.Email]; u!= nil{
-    WriteStatus(w,500,"user already exists")
+  err1,res:=GetKV([]byte(userPost.Email),[]byte("users"))
+  if res != nil {
+    log.Printf("err: %s",err1)
+    WriteStatus(w,500,"api newuser user already exists")
     return
   }
 
-  passwordHash:=GetMD5Hash(userPost.Password)
+//  runtime.Goshced()
+  
   user := &User{
-    Id:userIdCounter,
+    Id:GetUserId(),
+    Created: time.Now(), //int32(time.Now().Unix()),
     Email:userPost.Email,
-    PasswordHash:passwordHash,
+    PasswordHash:GetMD5Hash(userPost.Password),
   }
 
-  userIdCounter=userIdCounter+1;
 
-  userEmailStore[user.Email]=user
-  userIdStore[user.Id]=user
+  encoded, err := user.GobEncode()
+  if err != nil {
+    log.Printf("err: %s",err)
+    WriteStatus(w,500,"api newuser user error")
+    return
+  }
 
-  go saveStoresToFile()
+
+  err0:=UpdKV([]byte(userPost.Email),encoded,[]byte("users"))
+  if err0!=nil{
+    log.Printf("err: %s",err0)
+    WriteStatus(w,500,"api newuser addkv error")
+    return
+  }
+
 
   WriteStatus(w,200,"ok")
 }
@@ -130,18 +218,36 @@ func getUser(w http.ResponseWriter, r *http.Request){
     WriteStatus(w,500,"api user get error")
     return
   }
-  userGet:=UserGet{}
-  err = json.Unmarshal(content, &userGet) 
+  userGetReq:=UserGetReq{}
+  err = json.Unmarshal(content, &userGetReq) 
   if err != nil {
     log.Printf("err: %s",err)
     WriteStatus(w,500,"api user get data error")
     return
   }
 
-  log.Printf("userGet{%s}",userGet.Email)
-  if u:=userEmailStore[userGet.Email]; u!= nil{
+
+  log.Printf("userGet{%s}",userGetReq.Email)
+
+  _,res:=GetKV([]byte(userGetReq.Email),[]byte("users"))
+  if res != nil {
+
+    user := &User{}
+    err = user.GobDecode(res)
+    if err!=nil{
+      log.Printf("err: %s",err)
+      WriteStatus(w,500,"api user get decode error")
+      return
+    }
+/*
+    userGetResp := &UserGetResp{
+      Id:user.Id,
+      Created:user.Created,
+      Email:user.Email,
+    }
+*/
     w.WriteHeader(200)
-    WriteJSON(w,u)
+    err = WriteJSON(w,user)
     return
   }
 
@@ -179,5 +285,22 @@ func (e *Faas) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
   runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+  var err error
+  db, err = bolt.Open("data.db", 0600, nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  err = db.Update(func(tx *bolt.Tx) error {
+    _, err := tx.CreateBucketIfNotExists([]byte("users"))
+    if err != nil {
+        return fmt.Errorf("create bucket error: %s", err)
+    }
+    return nil
+  })  
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+  log.Println("http.ListenAndServe:8080");
   http.ListenAndServe(":8080", &Faas{})
 }
