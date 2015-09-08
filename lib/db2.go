e44b2c91695235987fcb5b6beb5ad1cdbf68a6a5
papercompute <papercompute@gmail.com>
2015-09-08 10:42:21 +0300

    hacking

---
+++ lib/db2.go
@@ -0,0 +1,61 @@
+package db2
+
+import (
+  "log"
+//  "strings"
+  "github.com/boltdb/bolt"
+
+)
+
+
+
+var db *bolt.DB
+
+func UpdBucket(bucket []byte) error {
+  err := db.Update(func(tx *bolt.Tx) error {
+    _, err := tx.CreateBucketIfNotExists(bucket)
+    return err
+  })
+  return err
+}
+
+func UpdKV(key []byte, value []byte, bucket []byte) error {
+  return db.Update(func(tx *bolt.Tx) error {
+    b := tx.Bucket(bucket)
+    return b.Put(key, value)
+  })
+}
+func GetKV(key []byte, bucket []byte) (error, []byte) {
+  var result []byte = nil
+  err := db.View(func(tx *bolt.Tx) error {
+    if b := tx.Bucket(bucket); b != nil {
+      result = b.Get(key)
+    }  
+    return nil
+  })
+  return err, result
+}
+
+func InitDB2(dbname *string, buckets []string){
+  var err error 
+  db, err = bolt.Open(*dbname, 0600, nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  err = db.Update(func(tx *bolt.Tx) error {
+  	for _,bucket:=range buckets{
+      _, err := tx.CreateBucketIfNotExists([]byte(bucket))
+      if err != nil {
+        log.Fatal(err)
+      }
+    }
+    return nil
+  })  
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func FinitDB2(){
+  db.Close()
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "flag"
+  "os"
 	"net/http"
 	"log"
 	"runtime"
@@ -13,29 +15,55 @@ import (
   "encoding/gob"
   "time"
 //	"regexp"
-  "github.com/boltdb/bolt"
   "bytes"
 //  "runtime"
+  "strings"
+  "./lib"
 )
 
-var db *bolt.DB
 
-func UpdKV(key []byte, value []byte, bucket []byte) error {
-  return db.Update(func(tx *bolt.Tx) error {
-    b := tx.Bucket(bucket)
-    return b.Put(key, value)
-  })
+/*
+example data config
+{  
+"hosts" : [
+  "127.0.0.1:9000",  "127.0.0.1:8000",
+  "127.0.0.1:9001",  "127.0.0.1:8001",
+  "127.0.0.1:9002",  "127.0.0.1:8002",
+
+  "127.0.0.1:9003",  "127.0.0.1:8003",
+  "127.0.0.1:9004",  "127.0.0.1:8004",
+  "127.0.0.1:9005",  "127.0.0.1:8005",
+
+  "127.0.0.1:9006",  "127.0.0.1:8006",
+  "127.0.0.1:9007",  "127.0.0.1:8007",
+  "127.0.0.1:9008",  "127.0.0.1:8008",
+
+  "127.0.0.1:9009",  "127.0.0.1:8009",
+  "127.0.0.1:9010",  "127.0.0.1:8010",
+  "127.0.0.1:9011",  "127.0.0.1:8011"
+],
+"replica" : "3"
 }
-func GetKV(key []byte, bucket []byte) (error, []byte) {
-  var result []byte = nil
-  err := db.View(func(tx *bolt.Tx) error {
-    if b := tx.Bucket(bucket); b != nil {
-      result = b.Get(key)
-    }  
-    return nil
-  })
-  return err, result
+*/
+
+type Config struct {
+  // key space (0..2^64)
+  Hosts []string  `json:"hosts"`
+  Replica uint  `json:"replica"`
+  Id uint  `json:"-"`  
+  Stora_host string  `json:"-"`  
+  Web_host string  `json:"-"`  
+  // hosts[(id % len(hosts))*replica]
 }
+
+var cfg Config
+/*
+func (c *Config) getUrlfromID(uint64 id)[]string {
+  i:=id%N
+}
+*/
+
+
 
 
 func GetMD5Hash(text string) string {
@@ -47,15 +75,15 @@ func GetMD5Hash(text string) string {
 // http.Error(rw, err.Error(), 500)
 
 func WriteStatus(w http.ResponseWriter, code int, status string) {
-  w.WriteHeader(code)
-  WriteJSON(w, map[string]string{
-    "status": status,
-  })
+    w.WriteHeader(code)
+    w.Header().Set("Content-Type", "application/json")
+    w.Write([]byte("{\"status\" : \""+status+"\"}"))
 }
+
 func WriteJSON(w http.ResponseWriter, v interface{}) error {
   b, err := json.MarshalIndent(v, "", "  ")
   if err != nil {
-    log.Printf("WriteJSON error %s",err)
+    log.Printf("WriteJSON error %v",err)
     return err
   }
   w.Header().Set("Content-Type", "application/json; charset=utf-8")
@@ -77,9 +105,10 @@ func ReadJSON(r *http.Request, v interface{}) error {
 }
 
 type Faas struct {}
+type Storage struct {}
 
 type User struct {
-  Id uint `json:"id"`
+  Id uint64 `json:"id"`
   Created time.Time  `json:"created"` // time.Now().Unix()
   Email string  `json:"email"`
   PasswordHash string `json:"-"`
@@ -137,7 +166,7 @@ func (u *User) GobDecode(buf []byte) error {
 
 
 
-type NewUserPost struct {
+type NewUserPostReq struct {
   Email string `json:"email"`
   Password string `json:"password"`
 }
@@ -145,23 +174,27 @@ type UserGetReq struct {
   Email string `json:"email"`
 }
 type UserGetResp struct {
-  Id uint `json:"id"`
+  Id uint64 `json:"id"`
   Created time.Time `json:"created"`
   Email string `json:"email"`
 }
 
-var userIdCounter uint = 127 
+var userIdCounter uint64 = 127 
 
-func GetUserId() uint{
+func GetUserId() uint64{
   uid := userIdCounter
   userIdCounter = userIdCounter + 1
   return uid  
 }
 
-func postNewUser(w http.ResponseWriter, r *http.Request){
-  userPost:=NewUserPost{}
 
-  if err := ReadJSON(r, &userPost); err != nil {
+// curl -v -XPOST -H "Content-Type: application/json" \
+// -d '{"email":"sobaka@drug.com","password":"123456789"}' \
+//'http://localhost:8080/api/v1/newuser'
+func postNewUser(w http.ResponseWriter, r *http.Request){
+  userPost:=NewUserPostReq{}
+  var err error
+  if err = ReadJSON(r, &userPost); err != nil {
     log.Printf("err: %s",err)
     WriteStatus(w,500,"api newuser post data error")
     return
@@ -176,15 +209,14 @@ func postNewUser(w http.ResponseWriter, r *http.Request){
     return
   }
 
-  err1,res:=GetKV([]byte(userPost.Email),[]byte("users"))
+  var res []byte
+  err,res=db2.GetKV([]byte(userPost.Email),[]byte("users"))
   if res != nil {
-    log.Printf("err: %s",err1)
+    log.Printf("err: %s",err)
     WriteStatus(w,500,"api newuser user already exists")
     return
   }
 
-//  runtime.Goshced()
-  
   user := &User{
     Id:GetUserId(),
     Created: time.Now(), //int32(time.Now().Unix()),
@@ -192,8 +224,8 @@ func postNewUser(w http.ResponseWriter, r *http.Request){
     PasswordHash:GetMD5Hash(userPost.Password),
   }
 
-
-  encoded, err := user.GobEncode()
+  var encoded []byte
+  encoded, err = user.GobEncode()
   if err != nil {
     log.Printf("err: %s",err)
     WriteStatus(w,500,"api newuser user error")
@@ -201,10 +233,10 @@ func postNewUser(w http.ResponseWriter, r *http.Request){
   }
 
 
-  err0:=UpdKV([]byte(userPost.Email),encoded,[]byte("users"))
-  if err0!=nil{
-    log.Printf("err: %s",err0)
-    WriteStatus(w,500,"api newuser addkv error")
+  err=db2.UpdKV([]byte(userPost.Email),encoded,[]byte("users"))
+  if err==nil{
+    log.Printf("err: %s",err)
+    WriteStatus(w,500,"api newuser update error")
     return
   }
 
@@ -229,7 +261,7 @@ func getUser(w http.ResponseWriter, r *http.Request){
 
   log.Printf("userGet{%s}",userGetReq.Email)
 
-  _,res:=GetKV([]byte(userGetReq.Email),[]byte("users"))
+  _,res:=db2.GetKV([]byte(userGetReq.Email),[]byte("users"))
   if res != nil {
 
     user := &User{}
@@ -255,11 +287,89 @@ func getUser(w http.ResponseWriter, r *http.Request){
 
 }
 
+
+/*
+func DecodeJson(r *http.Request, v interface{}) error {
+  content, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    return err
+  }
+  err = json.Unmarshal(content, v)
+  if err != nil {
+    return err
+  }
+  return nil
+}*/
+
+// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
+
+// curl -v -XPOST -d '123456789abcdef' 'http://localhost:8080/api/v1/set?bucket=mydata&key=mykey'
+func postTestKV(w http.ResponseWriter, r *http.Request){
+  var bucket,key string
+  w.Header().Set("Content-Type", "application/json")
+  if bucket = r.URL.Query().Get("bucket"); bucket == "" {
+    log.Printf("postTestKV /api/v1/set bucket error")
+    http.Error(w,"{\"status\" : \"bucket error\"}",400)
+    return
+  }
+  if key = r.URL.Query().Get("key"); key == "" {
+    log.Printf("postTestKV /api/v1/set key error")
+    http.Error(w,"{\"status\" : \"key error\"}",400)
+    return
+  }
+  err := db2.UpdBucket([]byte(bucket))
+  if err != nil {
+    log.Printf("postTestKV create bucket %s error %v",bucket,err)
+    http.Error(w,"{\"status\" : \"create bucket error\"}",400)
+    return
+  }
+  var value []byte  
+  if value, err = ioutil.ReadAll(r.Body); err != nil {
+    log.Printf("postTestKV read error %v",err)
+    http.Error(w,"{\"status\" : \"body error\"}",400)
+    return
+  }  
+  if err=db2.UpdKV([]byte(key),[]byte(value),[]byte(bucket)); err!=nil{
+    log.Printf("postTestKV update error %v",err)
+    http.Error(w,"{\"status\" : \"update error\"}",400)
+    return
+  }
+  w.WriteHeader(200);w.Write([]byte("{\"status\" : \"ok\"}"))
+}
+
+
+// curl -v -XGET 'http://localhost:8080/api/v1/get?bucket=mydata&key=mykey'
+func getTestKV(w http.ResponseWriter, r *http.Request){
+  var bucket,key string
+  if bucket = r.URL.Query().Get("bucket"); bucket == "" {
+    log.Printf("getTestKV /api/v1/get bucket error")
+    http.Error(w,"{\"status\" : \"bucket error\"}",400)
+    return
+  }
+  if key = r.URL.Query().Get("key"); key == "" {
+    log.Printf("getTestKV /api/v1/get key error")
+    http.Error(w, "{\"status\" : \"key error\"}", 400)
+    return
+  }
+  var err error
+  var value []byte  
+  if err,value=db2.GetKV([]byte(key),[]byte(bucket)); err != nil || value == nil {
+    log.Printf("getTestKV /api/v1/get key error")
+    http.Error(w,"{\"status\" : \"value error\"}",400)
+    return
+  }
+    w.WriteHeader(200)
+    w.Write(value) 
+}
+
+
 var fileServer = http.FileServer(http.Dir("public"))
 
 func (e *Faas) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
   switch r.URL.Path {
+
     case "/api/v1/newuser":
      if r.Method == "POST" {
        postNewUser(w,r)
@@ -268,6 +378,16 @@ func (e *Faas) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     case "/api/v1/user":
      if r.Method == "GET" {
        getUser(w,r)
+       return
+     }
+    case "/api/v1/set":
+     if r.Method == "POST" {
+       postTestKV(w,r)
+       return
+     }
+    case "/api/v1/get":
+     if r.Method == "GET" {
+       getTestKV(w,r)
        return
      }
 
@@ -282,25 +402,71 @@ func (e *Faas) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 
 
+func (e *Storage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+  http.NotFound(w, r)
+}
+
+
+/*
+
+  router := httprouter.New()
+  router.NotFound = http.FileServer(http.Dir("public")).ServeHTTP
+  log.Fatal(http.ListenAndServe(":8000", router))
+*/
 
 func main() {
-  runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+
+
+  var cfg_file_name* string = flag.String("cfg", "config.json", "config file")  
+  var db_file_name* string = flag.String("db", "data.db", "db file")  
+  var cfg_id* int = flag.Int("id", 0, "id")  
+
   var err error
-  db, err = bolt.Open("data.db", 0600, nil)
-  if err != nil {
-    log.Fatal(err)
+  runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+//  runtime.GOMAXPROCS(1)
+
+
+// read config
+  cfg_file, e := ioutil.ReadFile(*cfg_file_name)
+  if e != nil {
+    fmt.Printf("File error: %v\n", e)
+    os.Exit(1)
   }
-  err = db.Update(func(tx *bolt.Tx) error {
-    _, err := tx.CreateBucketIfNotExists([]byte("users"))
-    if err != nil {
-        return fmt.Errorf("create bucket error: %s", err)
-    }
-    return nil
-  })  
+  fmt.Printf("%s\n", string(cfg_file))
+
+  flag.Parse()
+
+  err = json.Unmarshal(cfg_file, &cfg)
   if err != nil {
-    log.Fatal(err)
+    fmt.Printf("Cfg file json error: %v\n", err)
+    os.Exit(1)
   }
-  defer db.Close()
-  log.Println("http.ListenAndServe:8080");
-  http.ListenAndServe(":8080", &Faas{})
+  cfg.Id = uint(*cfg_id)
+  cfg.Stora_host = cfg.Hosts[cfg.Id*2];
+  cfg.Web_host = cfg.Hosts[cfg.Id*2+1];
+
+  fmt.Printf("id: %d\n", *cfg_id)
+  fmt.Printf("replica: %d\n", cfg.Replica)
+  fmt.Printf("cfg: %s,%s\n", cfg.Stora_host,cfg.Web_host)
+
+  db2.InitDB2(db_file_name,[]string{"users","tests"})
+  defer db2.FinitDB2()
+
+// server setup
+  finish := make(chan bool)
+
+  go func() {
+    log.Println("http.ListenAndServe Stora_host "+cfg.Stora_host);
+    h:=strings.Split(cfg.Stora_host, ":")
+    http.ListenAndServe(":"+h[1], &Storage{})
+  }()
+
+  go func() {
+    log.Println("http.ListenAndServe Web_host "+cfg.Web_host);
+    h:=strings.Split(cfg.Web_host, ":")
+    http.ListenAndServe(":"+h[1], &Faas{})
+  }()
+
+  <-finish
 }
